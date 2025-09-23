ab6587df41829411788b14947ec8eff95eff4f0a
Lucky giri <luckygiri6266@gmail.com>
2025-09-23 12:38:55 +0530

    9th Day DSA

---
+++ RestoreString.java
@@ -0,0 +1,15 @@
+public class RestoreString {
+   
+    public String restoreString(String s, int[] indices) {
+        char[] res = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            res[indices[i]] = s.charAt(i);
+        }
+
+        return new String(res);
+    }
+}
+
+    
+
